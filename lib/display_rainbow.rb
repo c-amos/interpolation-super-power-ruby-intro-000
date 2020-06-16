5914d43d91e23a9ce318c88811926d7ce80a5bcf
Corbin A <corbin_amos@yahoo.com>
2020-06-16 16:37:33 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,9 +2,9 @@
 def display_rainbow(colors)
   print "R: #{colors[0]}, "
   print "O: #{colors[1]}, "
-  print "Y: #{colors[1]}, "
-  print "G: #{colors[2]}, "
-  print "B: #{colors[3]}, "
-  print "I: #{colors[4]}, "
-  print "V: #{colors[5]}"
+  print "Y: #{colors[2]}, "
+  print "G: #{colors[3]}, "
+  print "B: #{colors[4]}, "
+  print "I: #{colors[5]}, "
+  print "V: #{colors[6]}"
 end
